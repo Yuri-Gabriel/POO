f69a5a4ed5e8b4ec00f936f45857a67ee35c363a
/Yuri <67924730+Yuri-Gabriel@users.noreply.github.com>
2024-09-30 12:19:48 -0300

    Update App.java

--- App.java
+++ App.java
@@ -9,28 +9,28 @@ public class App {
 
         int acertos = 0;
         int player = 0;
-        int[] numeros = {
-            random.nextInt(1, 10),
-            random.nextInt(1, 10),
-            random.nextInt(1, 10)
-        };
+        int[] numeros = new int[3];
+        for(int i = 0; i < numeros.length; i++) {
+            int num = 1 + random.nextInt(10);
+            if(!numeroJaExiste(num, numeros)) {
+                numeros[i] =  num;
+            }
+        }
         
         System.out.println("Olá, eu pensei em 3 numero que estão entre 1 e 10, tente adivinhar");
         do {
             System.out.print("Digite um numero entre 1 e 10: ");
             player = scan.nextInt();
-            while(player < 1 || player > 10) {
+            while(player < 0 || player > 10) {
                 System.out.print("\nApenas numeros entre 1 e 10: ");
                 player = scan.nextInt();
             }
-            
-            
             for(int num : numeros) {
                 if(num == player) {
                     acertos++;
                 }
             }
-            System.out.println("Total de: " + acertos);
+            System.out.println("Total de acertos: " + acertos);
         } while(acertos < 3);
 
         System.out.println("Parabens, você acertou todos os numeros");
@@ -40,5 +40,14 @@ public class App {
         }
         System.out.println();
 
+    }
+
+    public static boolean numeroJaExiste(int n, int[] nums) {
+        for(int j = 0; j < nums.length; j++) {
+            if(n == nums[j]) {
+                return true;
+            }
+        }
+        return false;
     }
 }
